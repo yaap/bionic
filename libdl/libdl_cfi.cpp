627cb594f2d3424a0e3fecb903d5028e0dabb8e6
John Galt <johngaltfirstrun@gmail.com>
2026-05-01 12:10:51 -0400

    libdl: make cfi slowpath early return opt in
    This is a hack, and not all devices need this

--- libdl/libdl_cfi.cpp
+++ libdl/libdl_cfi.cpp
@@ -71,7 +71,9 @@ static uintptr_t cfi_check_addr(uint16_t v, void* Ptr) {
 }
 
 static inline void cfi_slowpath_common(uint64_t CallSiteTypeId, void* Ptr, void* DiagData) {
+#ifdef NEEDS_CFI_SLOWPATH_RETURN
   return;
+#endif
   uint16_t v = shadow_load(Ptr);
   switch (v) {
     case CFIShadow::kInvalidShadow:
